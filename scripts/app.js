ff3e7ddb579c5df77f78facbcb50e7e5710d467f
EMNCAntoine <126569349+EMNCAntoine@users.noreply.github.com>
2023-03-02 15:24:55 +0100

    v1

--- scripts/app.js
+++ scripts/app.js
@@ -38,128 +38,53 @@ function horsCampus()
 
 function resa()
 {
-    
-
-    // Initialisation de l'authentification
-const msalConfig = {
+  const msalConfig = {
     auth: {
         clientId: 'a4b505ae-05f8-4199-871a-37cf40a1ecb7',
-        redirectUri: 'https://emncantoine.github.io/resa/index.html',
+        // comment out if you use a multi-tenant AAD app
         authority: 'https://login.microsoftonline.com/ba18c5b0-7a13-44c1-97d2-9277ec61a255',
-        scopes: ['User.Read', 'Calendars.Read']
+        redirectUri: 'https://emncantoine.github.io/resa/'
     }
 };
-const msalInstance = new msal.PublicClientApplication(msalConfig);
+const msalRequest = { scopes: [] };
+function ensureScope (scope) {
+    if (!msalRequest.scopes.some((s) => s.toLowerCase() === scope.toLowerCase())) {
+        msalRequest.scopes.push(scope);
+    }
+}
+//Initialize MSAL client
+const msalClient = new msal.PublicClientApplication(msalConfig);
 
-// Authentification de l'utilisateur
-msalInstance.loginPopup()
-    .then(authResponse => {
-        // Récupération des événements du calendrier
-        const requestUrl = 'https://graph.microsoft.com/v1.0/me/events';
-        const accessToken = authResponse.accessToken;
-        console.log(accessToken);
-        const headers = new Headers({
-            'Authorization': `Bearer ${accessToken}`
-        });
-
-        fetch(requestUrl, { headers })
-            .then(response => response.json())
-            .then(data => {
-                // Traitement des événements
-                console.log(data);
-            });
-    })
-    .catch(error => {
-        console.log(error);
-    });
- /*   
-   // Configuration de l'authentification
-const msalConfig = {
-    auth: {
-      clientId: 'a4b505ae-05f8-4199-871a-37cf40a1ecb7',
-      redirectUri: 'https://emncantoine.github.io/resa/index.html',
-      authority: 'https://login.microsoftonline.com/ba18c5b0-7a13-44c1-97d2-9277ec61a255',
-    },
-    cache: {
-      cacheLocation: 'sessionStorage',
-      storeAuthStateInCookie: false,
-    },
-  };
-  const msalInstance = new msal.PublicClientApplication(msalConfig);
-  
-  // Récupération des événements du calendrier
-  const getUserEvents = async () => {
-    const account = msalInstance.getAccount();
+// Log the user in
+async function signIn() {
+    const authResult = await msalClient.loginPopup(msalRequest);
+    sessionStorage.setItem('msalAccount', authResult.account.username);
+}
+//Get token from Graph
+async function getToken() {
+    let account = sessionStorage.getItem('msalAccount');
     if (!account) {
-      throw new Error('Aucun compte utilisateur n\'est connecté.');
+        throw new Error(
+            'User info cleared from session. Please sign out and sign in again.');
     }
-  
-    const tokenRequest = {
-      scopes: ['https://graph.microsoft.com/Calendars.Read'],
-      account: account,
-    };
-  
     try {
-      const accessToken = await msalInstance.acquireTokenSilent(tokenRequest);
-      const eventsUrl = 'https://graph.microsoft.com/v1.0/users/abourgeois@emn-competences.fr/events';
-  
-      const response = await fetch(eventsUrl, {
-        headers: {
-          Authorization: `Bearer ${accessToken.accessToken}`,
-        },
-      });
-  
-      if (!response.ok) {
-        throw new Error('Impossible de récupérer les événements.');
-      }
-  
-      const events = await response.json();
-      console.log(events.value);
-      // Utilisez les données d'événement pour afficher ou traiter les événements de l'utilisateur.
-    } catch (error) {
-      console.log(error);
+        // First, attempt to get the token silently
+        const silentRequest = {
+            scopes: msalRequest.scopes,
+            account: msalClient.getAccountByUsername(account)
+        };
+
+        const silentResult = await msalClient.acquireTokenSilent(silentRequest);
+        return silentResult.accessToken;
+    } catch (silentError) {
+        // If silent requests fails with InteractionRequiredAuthError,
+        // attempt to get the token interactively
+        if (silentError instanceof msal.InteractionRequiredAuthError) {
+            const interactiveResult = await msalClient.acquireTokenPopup(msalRequest);
+            return interactiveResult.accessToken;
+        } else {
+            throw silentError;
+        }
     }
-  };
-  
-  // Authentification de l'utilisateur
-  msalInstance.loginPopup({
-    scopes: ['User.Read'],
-  }).then(() => {
-    getUserEvents();
-  });
-  
-  const config = {
-    auth: {
-      clientId: "a4b505ae-05f8-4199-871a-37cf40a1ecb7",
-      redirectUri: "https://emncantoine.github.io/resa/index.html", //defaults to application start page
-      postLogoutRedirectUri: "https://emncantoine.github.io/resa/index.html",
-    },
-  };
-  
-  const user = "abourgeois@emn-competences.fr";
-  const myaccount = msalInstance.getAccountByUsername(user);
-
-  const loginRequest = {
-    scopes: ["User.ReadWrite"],
-    account: myaccount
-  };
-
-  const loginResponse = msalInstance.ssoSilent(loginRequest);
-  console.log(loginResponse);
-/*
-  let accountId = "";
-  
-  const myMsal = new PublicClientApplication(config);
-  
-  myMsal
-    .loginPopup(loginRequest)
-    .then(function (loginResponse) {
-      accountId = loginResponse.account.homeAccountId;
-      console.log('Connexion OK')
-    })
-    .catch(function (error) {
-      //login failure
-      console.log(error);
-    });
-*/
+}
 }
